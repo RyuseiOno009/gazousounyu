6a4428a38fd54f07cc7b4fd76ebf1fd90177fa54
Tashiki009 <52692397+Tashiki009@users.noreply.github.com>
2019-10-11 17:49:31 +0900

    unityファイル

---
+++ gazoukakuninn/Assets/pic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        byte[] bytes = System.Convert.FromBase64String(data: image / png; base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABQCAYAAADvCdDvAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAhxJREFUeNrsnVGRgzAYBnGABCQgAQmVgISTgIRKiIRKQEIlIAEJPTqTznBMaRMOko9kd4b341 + yB / 89XFEAAAAAwB / q6bowBh0ZhjHoyBimq2QUGjJGUqXB80Tcp + vGKHRkjKRKR8aDVGnJIFUCGCuDVAnJIFViMkiVkAxSFZl2JoNUickgVUIySJWQDFIVkdeykFSJyiBVQjJIVSSqFRmkKgLzZeEyVRXj0ZDxvH4Yj46MnvGEZ00GqYqAWZFBqsRkkCohGcmkqjnJl + z1g4ykUlXZo64spf0iI7lUdfatpTyhjCTfqkp7Y2pSvslI + q2qszeoIqVxkJH8W9Uwy0Ad8edY29xm9wHYLm64FpWR1QfgEFGKq4ysPgCbN2kIIaV0lJHlrqp / M4TLwTLuDjKy3VWtveG0kWVkvau6BZDiIyP7tXr1YTh7SekdZbBWt5gDpRgPGazVZ6fk05uPCSCDvwCurFT2kuIjg1Rt / D4wO8ldXnfGv32Q36S0njIeRdx9mvwpGRwGeCveb4q3yOgY + z5P + HJ9v0UGqXJk8JRSb5BBqjy4eD7lI6k6nn7jU0 + qDqI5UAipEjolpOqfKxVSJYYhVWmeElIVeKVCqgKvVMaCVCVxSkjVgadkKEiVFL4LRFIVANdTQqoC4bJ4JFViKxVSFZiGVJ3jlJCqiNSkSo / 54vHKOOLzWjzyT1KEuNpf8gAAAAAAO / ArwABV3ssrwRXpEAAAAABJRU5ErkJggg ==);
+        var texture = new Texture2D(1, 1);
+        texture.LoadImage(bytes);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
